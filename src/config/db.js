6f428527b0d70791f354454ff70aeb6cea1ef739
gourav-yadav-dev <yadavgourav263@gmail.com>
2026-01-30 22:44:16 +0530

    Initial backend setup with database schema

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const mySql = require('mysql2/promise')
+
+const pool = mySql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 5
+})
+
+module.exports = pool
